3bbcc0251d167fb1dc996adb097849cc3b34de4e
johnsonxin <johnsonxin@tencent.com>
2021-08-23 15:42:40 +0800

    simple_factory

---
+++ simple_factory/simple_factory.go
@@ -0,0 +1,35 @@
+package simple_factory
+
+import "fmt"
+/*
+简单的工厂模式
+go 中没有构造函数，通过NewXXX函数来初始化类对象。
+封装了实现细节，仅仅暴露了API接口以及NewAPI函数。
+*/
+type API interface {
+	Say(name string) string
+}
+
+type hiAPI struct {
+}
+
+func (*hiAPI) Say(name string) string {
+	return fmt.Sprintf("hi, %s", name)
+}
+
+type helloAPI struct {
+}
+
+func (*helloAPI) Say(name string) string {
+	return fmt.Sprintf("hello, %s", name)
+}
+
+func NewAPI(t int)API  {
+	if t == 1{
+		return &hiAPI{}
+	}
+	if t == 2 {
+		return &helloAPI{}
+	}
+	return nil
+}
---
+++ simple_factory/simple_factory_test.go
@@ -0,0 +1,20 @@
+package simple_factory
+
+import (
+	"testing"
+)
+
+func TestNewAPI1(t *testing.T) {
+	api :=NewAPI(1)
+	s:=api.Say("Johnson")
+	if s!= "hi, Johnson"{
+		t.Fatal("Type1 test fail")
+	}
+}
+func TestNewAPI2(t *testing.T) {
+	api :=NewAPI(2)
+	s:=api.Say("Johnson")
+	if s!= "hello, Johnson"{
+		t.Fatal("Type2 test fail")
+	}
+}
